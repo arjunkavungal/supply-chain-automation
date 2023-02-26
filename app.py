4d73a52ccf00cb06142724ae8a5ca31648e9cf15
Arjun Kavungal <47582621+yimen3@users.noreply.github.com>
2023-02-25 23:04:25 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from io import StringIO
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 * 1024 * 1024
+@app.route('/')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_a_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      #df = pd.read_csv(StringIO(f))
+      df = pd.read_csv(f.filename)
+      df['Date'] = pd.to_datetime(df['Date'])
+      df['Day'] = df.Date.dt.day
+      s = ""
+      for i in df['Product'].unique():
+        tire = df[df['Product'] == i]
+        gb = tire.groupby('Month').count()
+        x = [[12],[1],[11],[10],[9]]
+        y = gb['Product']
+        model = LinearRegression().fit(x, y)
+        s += i + str(model.predict([[13]])[0])
+      return s
+   return s
+
+app.run(host='0.0.0.0',port=81)
